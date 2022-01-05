3f8007621df68494c3a49edc8e7899f1ba1b7f5d
hagleyson <hagleyson@gmail.com>
2022-01-04 22:37:48 -0300

    using useMemo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 
 function App() {
+  const [listTitle, setListTitle] = useState("My List");
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
   const [showParagraph, seShowParagraph] = useState(false);
   const toggleParagraphHandler = useCallback(() => {
     seShowParagraph((prevShowParagraph) => !prevShowParagraph);
   }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
 
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
